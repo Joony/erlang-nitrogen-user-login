fd75098f77aa2a8a2284388e4adb75443db8041b
Joony <jonathan.mcallister@gmail.com>
2009-07-29 11:56:08 +0200

    Added success and error pages for the email verification

--- src/pages/web_users_verify_email.erl
+++ src/pages/web_users_verify_email.erl
@@ -6,20 +6,30 @@ main() ->
     [Code] = wf:q("key"),
     case db_users:verify_email(Code) of
 	{atomic,ok} ->
-	    io:format("Everything looks ok~n");
+	    Validated = true,
+	    %io:format("Everything looks ok~n");
 	{aborted, "invalid verification code"} ->
-	    io:format("Invalid verification code~n");
+	    Validated = false,
+	    wf:redirect("/web/errors/verification/email"),
+	    %io:format("Invalid verification code~n");
 	_ ->
-	    io:format("Something went wrong~n")
+	    Validated = false,
+	    wf:redirect("/web/errors/unexpected"),
+	    %io:format("Something went wrong~n")
     end,
-
-#template { file="./wwwroot/template.html"}.
+    #template { file="./wwwroot/validation.html", bindings=[{'Validated', Validated}] }.
     
 
 title() ->
 	"web_users_verify_email".
 
-body() ->
-	#label{text="web_users_verify_email body."}.
+body(Validated) ->
+    io:format("Validated = ~s~n", [Validated]),
+    case Validated of
+	true ->
+	    #label{text="Your email address has been successfully validated"};
+	_ ->
+	    #label{text="There was a problem validating your email address"}
+    end.
 	
 event(_) -> ok.
